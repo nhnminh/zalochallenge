f07005f5271791f33cbe161fd5cb7beca472c1c5
Minh Nguyen <nhnminh@apcs.vn>
2018-08-25 07:18:32 +0700

    Create description file with labels

--- Voice/src/data_loading.py
+++ Voice/src/data_loading.py
@@ -9,5 +9,33 @@ directory = os.getcwd()
 print(directory)
 
 
-data_path = 'data\\train\\'
+data_path = directory + '\\Voice\\data\\unzipped\\train'
+
+
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+
+df = pd.DataFrame()
+subdirs = get_immediate_subdirectories(data_path)
+for subdir in subdirs:
+    filenames = os.listdir(data_path + "\\" + subdir)
+    gender = subdir.split("_")[0]
+    accent = subdir.split("_")[1]
+    df1 = pd.DataFrame({
+            'Filename' : filenames,
+            'Foldername' : subdir,
+            'Gender' : gender,
+            'Accent' : accent
+
+        })
+    print(df1.head())
+    df = df.append(df1, ignore_index=True)
+    
+
+print(df)
+
+df.to_csv("description.csv")
+    
 
